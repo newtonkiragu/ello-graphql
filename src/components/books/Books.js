4396b07e1cafee77f3cb77b8c8d8c9cd6d247ce3
Karanu Newton <karanunewton4@gmail.com>
2022-04-11 20:52:41 +0300

    rearranged content component and code cleanup

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,7 +1,7 @@
 import { React, useMemo, useState } from 'react';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import Content from "../Content";
+import Content from "../content/Content";
 import { useQuery } from '@apollo/client';
 
 let PageSize = 2;
@@ -21,30 +21,17 @@ const query = gql`
 `;
 const Books = () => {
     const {loading, data, error} = useQuery(query)
-    // const pagesData = data.books.pages
-
-    // const currentTokensData = useMemo(() => {
-    //     const firstPageIndex = (currentPage - 1) * PageSize;
-    //     const lastPageIndex = firstPageIndex + PageSize;
-    //     return pagesData.slice(firstPageIndex, lastPageIndex);
-    // }, [currentPage]);
 
     if (loading) return <p>Loading Masterpieces ...</p>
 
     if (error) return <pre>{error.message}</pre>
-
-    // if (data) {
-    //     const pagesData = data.books.pages
-    // }
 
     if (data) {
         return(
             <Content data={data.book} pageSize={PageSize}/>
         )
     }
-    // return (
-    //
-    // )
+
 
 };
 export default Books;
