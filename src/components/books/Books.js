b3071a0534b541356d26d40f260c2f2b5f73d826
Karanu Newton <karanunewton4@gmail.com>
2022-04-12 00:23:46 +0300

    removed unused vars

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,4 +1,4 @@
-import { React, useMemo } from 'react';
+import { React } from 'react';
 import gql from "graphql-tag";
 import Content from "../content/Content";
 import { useQuery } from '@apollo/client';
@@ -21,7 +21,7 @@ const query = gql`
 const Books = () => {
     const {loading, data, error} = useQuery(query)
 
-    if (loading) return <p>Loading Masterpieces ...</p>
+    if (loading) return <p>Loading Book ...</p>
 
     if (error) return <pre>{error.message}</pre>
 
