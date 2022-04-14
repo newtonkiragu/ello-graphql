fbd0cb835628fa3f05efd9d01934655be3952455
Karanu Newton <karanunewton4@gmail.com>
2022-04-14 12:38:59 +0300

    created loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,28 @@
+import './loading.css';
+
+function Loading() {
+    return(
+
+    <div className="loader">
+        <div className="loadingio-spinner-spinner-g2364ngqmdg">
+            <div className="ldio-r9un1yjyct">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+        <h2 className="loadtitle">Loading Book...</h2>
+    </div>
+    )
+}
+
+export default Loading;
