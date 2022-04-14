cda92a002709ebb654dab28948216dbc85ec4562
Karanu Newton <karanunewton4@gmail.com>
2022-04-14 11:58:49 +0300

    prettified code && removed console log

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,4 +1,4 @@
-import {React, useMemo, useState} from "react";
+import { React, useMemo, useState } from 'react';
 import Pagination from '../pagination/Pagination';
 import Modal from '../modals/Modal';
 
@@ -6,70 +6,60 @@ import './Content.css';
 
 let PageSize = 2;
 const Content = (props) => {
-    const [currentPage, setCurrentPage] = useState(1);
-    let [show, setShow] = useState( false);
-    let [modalData, setModalData] = useState();
+  const [currentPage, setCurrentPage] = useState(1);
+  let [show, setShow] = useState(false);
+  let [modalData, setModalData] = useState();
 
-    const toggleModal = () => {
-        setShow(!show);
-        modalData = {};
-    };
-
-    const toggleModalData = (data) => {
-      setShow(true);
-      setModalData(data);
-
+  const toggleModal = () => {
+    setShow(!show);
+    modalData = {};
   };
 
+  const toggleModalData = (data) => {
+    setShow(true);
+    setModalData(data);
+  };
 
-    const currentTokensData = useMemo(() => {
-        const firstPageIndex = (currentPage - 1) * PageSize;
-        const lastPageIndex = firstPageIndex + PageSize;
+  const currentTokensData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
 
-        return props.data.pages.slice(firstPageIndex, lastPageIndex);
-    }, [currentPage]);
+    return props.data.pages.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
 
-    return(
-        <div className="container">
-            <Pagination
-                className="pagination-bar"
-                currentPage={currentPage}
-                totalCount={props.data.pages.length}
-                pageSize={PageSize}
-                onPageChange={page => setCurrentPage(page)}
-            />
+  return (
+    <div className="container">
+      <Pagination
+        className="pagination-bar"
+        currentPage={currentPage}
+        totalCount={props.data.pages.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
 
-            <div className="wrapper">
-                {currentTokensData.map(item => {
-                    return(
-                        <div className="post">
-                            {item.tokens.map((tokens, i) => {
-                                console.log(tokens.position);
-                                return(
-                                    <div key={i}
-                                    >
-                                        <h1 onClick={() => toggleModalData(tokens)}>{`${tokens.value}`}</h1>
+      <div className="wrapper">
+        {currentTokensData.map((item) => {
+          return (
+            <div className="post">
+              {item.tokens.map((tokens, i) => {
+                return (
+                  <div key={i}>
+                    <h1 onClick={() => toggleModalData(tokens)}>{`${tokens.value}`}</h1>
 
-                                        <Modal
-                                            show={show}
-                                            modalData={modalData}
-                                            closeCallback={toggleModal}
-                                            customClass="custom_modal_class"
-                                        >
-                                        </Modal>
-                                    </div>
-                                )
-                            })}
-                        </div>
-
-
-                    )
-                })}
+                    <Modal
+                      show={show}
+                      modalData={modalData}
+                      closeCallback={toggleModal}
+                      customClass="custom_modal_class"
+                    ></Modal>
+                  </div>
+                );
+              })}
             </div>
-
-        </div>
-
-    )
-
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 export default Content;
--- src/components/modals/Modal.js
+++ src/components/modals/Modal.js
@@ -1,39 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Modal.css'
+import './Modal.css';
 
 const Modal = ({ modalData, customClass, show, closeCallback }) => {
-    console.log(modalData);
-    if (!show) {
-        return null
-    }
-    return (
-        <div className={`modal ${customClass}`}>
-            <div className="overlay" onClick={closeCallback}></div>
-            <div className="modal_content">
-                <h2><strong>{`${modalData.position}`}</strong></h2>
-                <button title="Close" className="close_modal" onClick={closeCallback}>
-                    close
-                </button>
-            </div>
-        </div>
-    )
-
+  if (!show) {
+    return null;
+  }
+  return (
+    <div className={`modal ${customClass}`}>
+      <div className="overlay" onClick={closeCallback}></div>
+      <div className="modal_content">
+        <h2>
+          <strong>{`${modalData.position}`}</strong>
+        </h2>
+        <button title="Close" className="close_modal" onClick={closeCallback}>
+          close
+        </button>
+      </div>
+    </div>
+  );
 };
 
 Modal.propTypes = {
-    modalData: PropTypes.element,
-    customClass: PropTypes.string,
-    show: PropTypes.bool,
-    closeCallback: PropTypes.func,
+  modalData: PropTypes.element,
+  customClass: PropTypes.string,
+  show: PropTypes.bool,
+  closeCallback: PropTypes.func
 };
 
 Modal.defaultProps = {
-    modalData: <div>Empty Modal</div>,
-    customClass: '',
-    show: false,
-    closeCallback: () => (false)
+  modalData: <div>Empty Modal</div>,
+  customClass: '',
+  show: false,
+  closeCallback: () => false
 };
 
 export default Modal;
